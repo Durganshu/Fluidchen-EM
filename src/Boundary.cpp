17ad99529622e3ccfc5e6985dfb009318a700568
Durganshu <durganshumishra@gmail.com>
2022-05-05 00:45:13 +0200

    Updated .gitignore to avoid storing sourcetrail files; commented incorrect lines in boundary.cpp

--- src/Boundary.cpp
+++ src/Boundary.cpp
@@ -9,22 +9,22 @@ FixedWallBoundary::FixedWallBoundary(std::vector<Cell *> cells, std::map<int, do
 
 void FixedWallBoundary::apply(Fields &field) {
 
-    for (auto &elem : _cells) {
-        int i = elem.i();
-        int j = elem.j();
+    // for (auto &elem : _cells) {
+    //     int i = elem.i();
+    //     int j = elem.j();
 
-        if (i == 0 || i == _U.imax())) {
-            if (j == 0) {
-                _U[i][j] = -_U[i][j + 1];
-                _V[i][j] = -_V[i+1][j]
-                return;
-            }
-            if (j == _U.jmax()) {
-                _U[i][j] = -_U[i][j - 1];
+    //     if (i == 0 || i == _U.imax())) {
+    //         if (j == 0) {
+    //             _U[i][j] = -_U[i][j + 1];
+    //             _V[i][j] = -_V[i+1][j]
+    //             return;
+    //         }
+    //         if (j == _U.jmax()) {
+    //             _U[i][j] = -_U[i][j - 1];
                 
-            }
-        }
-    }
+    //         }conda 
+    //     }
+    // }
 }
 
 MovingWallBoundary::MovingWallBoundary(std::vector<Cell *> cells, double wall_velocity) : _cells(cells) {
