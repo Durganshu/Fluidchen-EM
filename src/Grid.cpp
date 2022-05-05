de06508d1d9995faf4610007aacff63a279ed9bb
Gerasimos Chourdakis <makishourdakis@gmail.com>
2022-05-05 11:42:17 +0200

    Format code with clang-format-11

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -48,9 +48,7 @@ void Grid::assign_cell_types(std::vector<std::vector<int>> &geometry_data) {
     int j = 0;
 
     for (int j_geom = _domain.jmin; j_geom < _domain.jmax; ++j_geom) {
-        {
-            i = 0;
-        }
+        { i = 0; }
         for (int i_geom = _domain.imin; i_geom < _domain.imax; ++i_geom) {
             if (geometry_data.at(i_geom).at(j_geom) == 0) {
                 _cells(i, j) = Cell(i, j, cell_type::FLUID);
