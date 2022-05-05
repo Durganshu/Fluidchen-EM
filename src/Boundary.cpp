5c8d64b6a2e1d6432b7702be70ca33a39ce590e9
Yashas Tejaskumar Gandhi <y.gandhi@tum.de>
2022-05-05 14:06:41 +0200

    Partially completed apply()

--- src/Boundary.cpp
+++ src/Boundary.cpp
@@ -9,22 +9,24 @@ FixedWallBoundary::FixedWallBoundary(std::vector<Cell *> cells, std::map<int, do
 
 void FixedWallBoundary::apply(Fields &field) {
 
-    // for (auto &elem : _cells) {
-    //     int i = elem.i();
-    //     int j = elem.j();
+    for (auto &elem : _cells) {
+        int i = elem->i();
+        int j = elem->j();
 
-    //     if (i == 0 || i == _U.imax())) {
-    //         if (j == 0) {
-    //             _U[i][j] = -_U[i][j + 1];
-    //             _V[i][j] = -_V[i+1][j]
-    //             return;
-    //         }
-    //         if (j == _U.jmax()) {
-    //             _U[i][j] = -_U[i][j - 1];
-                
-    //         }conda 
-    //     }
-    // }
+        if (i == 0) {
+            field.u(i, j) = 0;
+            field.v(i, j) = -field.v(i + 1, j);
+            return;
+        }
+
+        if (j == 0) {
+            field.u(i, j) = -field.u(i, j + 1);
+            field.v(i, j) = 0;
+            return;
+        }
+
+        //if (i==field.)
+    }
 }
 
 MovingWallBoundary::MovingWallBoundary(std::vector<Cell *> cells, double wall_velocity) : _cells(cells) {
