46ddb3bd4e23fed3336870d4e7f49feffe658f34
Yashas Gandhi <y.gandhi17@tum.de>
2022-06-11 09:03:48 +0200

    Fixed minor issues

--- include/Communication.hpp
+++ include/Communication.hpp
@@ -1,12 +1,13 @@
 #include <mpi.h>
 #include "Datastructures.hpp"
+#include "Domain.hpp"
 
 /**
  * @brief Communicates ghost layer values with neighbours
  *
  * @param[in] Field to be applied
  */
-static void communicate(Matrix &data, Domain &domain);
+static void communicate(Matrix<double> &data, Domain &domain);
 
 /**
  * @brief Returns the minimum time-step among all processes
--- src/Communication.cpp
+++ src/Communication.cpp
@@ -1,7 +1,7 @@
 #include "Communication.hpp"
 #include <vector>
 
-static void communicate(Matrix &data, Domain &domain) {
+static void communicate(Matrix<double> &data, Domain &domain) {
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -11,8 +11,8 @@ static void communicate(Matrix &data, Domain &domain) {
     if (domain.neighbours[0] != -1) {
         std::vector<double> send = data.get_col(1);
         std::vector<double> recv;
-        MPI_Sendrecv(&send, domain.size_y + 2, MPI_Double, domain.neighbours[0], 1, &recv, domain.size_y + 2,
-                     MPI_Double, domain.neighbours[0], 2, MPI_COMM_WORLD, &status);
+        MPI_Sendrecv(&send, domain.size_y + 2, MPI_DOUBLE, domain.neighbours[0], 1, &recv, domain.size_y + 2,
+                     MPI_DOUBLE, domain.neighbours[0], 2, MPI_COMM_WORLD, &status);
         data.set_col(recv, 0);
     }
 
@@ -20,8 +20,8 @@ static void communicate(Matrix &data, Domain &domain) {
     if (domain.neighbours[1] != -1) {
         std::vector<double> send = data.get_col(domain.imax);
         std::vector<double> recv;
-        MPI_Sendrecv(&send, domain.size_y + 2, MPI_Double, domain.neighbours[1], 2, &recv, domain.size_y + 2,
-                     MPI_Double, domain.neighbours[1], 1, MPI_COMM_WORLD, &status);
+        MPI_Sendrecv(&send, domain.size_y + 2, MPI_DOUBLE, domain.neighbours[1], 2, &recv, domain.size_y + 2,
+                     MPI_DOUBLE, domain.neighbours[1], 1, MPI_COMM_WORLD, &status);
         data.set_col(recv, domain.imax + 1);
     }
 
@@ -29,8 +29,8 @@ static void communicate(Matrix &data, Domain &domain) {
     if (domain.neighbours[2] != -1) {
         std::vector<double> send = data.get_row(domain.jmax);
         std::vector<double> recv;
-        MPI_Sendrecv(&send, domain.size_x + 2, MPI_Double, domain.neighbours[2], 3, &recv, domain.size_x + 2,
-                     MPI_Double, domain.neighbours[2], 4, MPI_COMM_WORLD, &status);
+        MPI_Sendrecv(&send, domain.size_x + 2, MPI_DOUBLE, domain.neighbours[2], 3, &recv, domain.size_x + 2,
+                     MPI_DOUBLE, domain.neighbours[2], 4, MPI_COMM_WORLD, &status);
         data.set_row(recv, domain.jmax + 1);
     }
 
@@ -38,8 +38,8 @@ static void communicate(Matrix &data, Domain &domain) {
     if (domain.neighbours[3] != -1) {
         std::vector<double> send = data.get_row(1);
         std::vector<double> recv;
-        MPI_Sendrecv(&send, domain.size_x + 2, MPI_Double, domain.neighbours[3], 4, &recv, domain.size_x + 2,
-                     MPI_Double, domain.neighbours[3], 3, MPI_COMM_WORLD, &status);
+        MPI_Sendrecv(&send, domain.size_x + 2, MPI_DOUBLE, domain.neighbours[3], 4, &recv, domain.size_x + 2,
+                     MPI_DOUBLE, domain.neighbours[3], 3, MPI_COMM_WORLD, &status);
         data.set_row(recv, 0);
     }
 }
